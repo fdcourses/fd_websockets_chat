4ac8414d30d652e56d6711ba005ebad5f212ee48
Kirill <freshjs.ftd@gmail.com>
2021-11-23 15:23:38 +0200

    Add redux-devtools-extension

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,15 @@
-import {createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import createSagaMW from 'redux-saga';
-
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers';
 import rootSaga from '../sagas';
 
 const sagaMW = createSagaMW();
 
-const store = createStore(rootReducer, applyMiddleware(sagaMW));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMW))
+);
 
 sagaMW.run(rootSaga);
 
